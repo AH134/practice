124ad455e41380615e0846159ae0010aba409af5
Andy <github.com.uo5n8@aleeas.com>
2023-04-07 18:44:15 -0700

    learned never to mutate state directly

--- react-practice/fso-course/src/App.jsx
+++ react-practice/fso-course/src/App.jsx
@@ -1,30 +1,44 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - age;
+import { useState } from "react";
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div> the app is used by pressing the butts </div>;
+  }
+  return <div> button press history: {props.allClicks.join(" ")} </div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}> {text} </button>
+);
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    const updatedLeft = left + 1;
+    setAll(allClicks.concat("L"));
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
+  const handleRightClick = () => {
+    const updatedRight = right + 1;
+    setAll(allClicks.concat("R"));
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   };
 
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably forn in {bornYear()}</p>
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
-
-const App = () => {
-  const name = "Peter";
-  const age = 10;
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
-
 export default App;
