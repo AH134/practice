4293daa944925c8311bcbfce72bcc9e22490bd7d
Andy <github.com.uo5n8@aleeas.com>
2023-04-09 23:39:30 -0700

    exercise 2.1-2.5

--- react-practice/fso-course/src/App.jsx
+++ react-practice/fso-course/src/App.jsx
@@ -1,57 +1,57 @@
-import { useState } from "react";
+import Course from "./components/Course";
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
-};
-
-const MostVotedAnecdote = ({ anecdote, votes }) => {
-  const mostVotes = Math.max(...votes);
-  const index = votes.indexOf(mostVotes);
-
-  return (
-    <p>
-      {anecdote[index]} has {mostVotes} votes
-    </p>
-  );
-};
 const App = () => {
-  const points = new Array(8).fill(0);
-
-  const anecdotes = [
-    "If it hurts, do it more often.",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
-    "The only way to go fast, is to go well.",
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
   ];
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(points);
-
-  const handleNextAnecdote = () => {
-    const randomNumber = Math.round(Math.random() * anecdotes.length);
-    setSelected(randomNumber);
-  };
-
-  const handleVote = () => {
-    const copyPoints = [...votes];
-    copyPoints[selected] += 1;
-    setVotes(copyPoints);
-  };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      {anecdotes[selected]}
-      <br />
-
-      <Button handleClick={handleVote} text="vote" />
-      <Button handleClick={handleNextAnecdote} text="next anecdote" />
-
-      <h1>Anecdote with most votes</h1>
-      <MostVotedAnecdote anecdote={anecdotes} votes={votes} />
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
     </div>
   );
 };
