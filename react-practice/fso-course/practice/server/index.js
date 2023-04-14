1c3379c791a10e245079cec354fedcaa375c4622
Andy <github.com.uo5n8@aleeas.com>
2023-04-13 19:58:14 -0700

    added static file to backend

--- react-practice/fso-course/practice/server/index.js
+++ react-practice/fso-course/practice/server/index.js
@@ -13,6 +13,7 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 app.use(reqLogger);
