09f8ed433ee147eccfbb8e65a4b42c138c0d1aca
Andy <github.com.uo5n8@aleeas.com>
2023-04-23 16:22:45 -0700

    Learned about user admin/pass hash

--- react-practice/fso-course/practice/server/controllers/notes.js
+++ react-practice/fso-course/practice/server/controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
-notesRouter.get("/", async (rea, res) => {
-  const notes = await Note.find({});
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
 
   res.json(notes);
 });
@@ -10,12 +11,18 @@ notesRouter.get("/", async (rea, res) => {
 notesRouter.post("/", async (req, res) => {
   const body = req.body;
 
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
   res.status(201).json(savedNote);
 });
 
