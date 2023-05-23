fede2e4b20e562ed8dade091b1d6421ce5c3d112
andy <github.com.uo5n8@aleeas.com>
2023-05-23 14:08:40 -0700

    Exercise 6.1-6.2

---
+++ react-practice/fso-course/unicafe/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { legacy_createStore } from "redux";
+import reducer from "./reducers/reducer";
+import App from "./App";
+
+const store = legacy_createStore(reducer);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(
+    <React.StrictMode>
+      <App store={store} />
+    </React.StrictMode>
+  );
+};
+
+renderApp();
+store.subscribe(renderApp);
