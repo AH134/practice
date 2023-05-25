b4b6f99a38b428d260e9c030147f52cb3ab8535a
andy <github.com.uo5n8@aleeas.com>
2023-05-25 11:10:53 -0700

    learned combined reducers

--- react-practice/fso-course/simple-note-app/src/main.jsx
+++ react-practice/fso-course/simple-note-app/src/main.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore } from "redux";
+import { legacy_createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App.jsx";
-import noteReducer from "./reducers/noteReducer.jsx";
+import noteReducer from "./reducers/noteReducer.js";
+import filterReducer from "./reducers/filterReducer.js";
 
-const store = legacy_createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+import { filterChange } from "./reducers/filterReducer.js";
+import { createNote } from "./reducers/noteReducer.js";
+const store = legacy_createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange("IMPORTANT"));
+store.dispatch(
+  createNote("combineReducers form one reduceer from many simple reducers")
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
