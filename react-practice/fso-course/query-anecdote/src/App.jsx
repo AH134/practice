030b64dbfb09b1ee82bb58f993168560543cde25
andy <github.com.uo5n8@aleeas.com>
2023-06-01 11:57:29 -0700

    Exercise 6.20-6.21

--- react-practice/fso-course/query-anecdote/src/App.jsx
+++ react-practice/fso-course/query-anecdote/src/App.jsx
@@ -1,18 +1,41 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getAnecdotes, updateAnecdote } from "./services/anecdotes";
+
 const App = () => {
+  const queryClient = useQueryClient();
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData("anecdotes");
+      queryClient.setQueryData(
+        "anecdotes",
+        anecdotes.map((anecdote) =>
+          anecdote.id !== newAnecdote.id ? anecdote : newAnecdote
+        )
+      );
+    },
+  });
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
-  const anecdotes = [
-    {
-      content: "If it hurts, do it more often",
-      id: "47145",
-      votes: 0,
-    },
-  ];
+  const result = useQuery("anecdotes", getAnecdotes, {
+    retry: false,
+  });
+
+  switch (result.status) {
+    case "loading":
+      return <div>Loading...</div>;
+    case "error":
+      return (
+        <div>Anecdote service not available due to problems in server</div>
+      );
+  }
+  console.log(result.data);
+
+  const anecdotes = result.data;
 
   return (
     <div>
--- react-practice/fso-course/query-anecdote/src/components/AnecdoteForm.jsx
+++ react-practice/fso-course/query-anecdote/src/components/AnecdoteForm.jsx
@@ -1,9 +1,21 @@
+import { addAnecdote } from "../services/anecdotes";
+import { useMutation, useQueryClient } from "react-query";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation(addAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdote = queryClient.getQueryData("anecdotes");
+      queryClient.setQueryData("anecdotes", anecdote.concat(newAnecdote));
+    },
+  });
+
   const onCreate = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
     console.log("new anecdote");
+    newAnecdoteMutation.mutate({ content, votes: 0 });
   };
 
   return (
