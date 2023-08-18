232528020387afa8cc68de306d455fb669b4daf4
Andy <github.com.uo5n8@aleeas.com>
2023-08-17 22:56:20 -0700

    initial commit

---
+++ ts-job-board/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <hr style={{ margin: "0 1rem 0 1rem", opacity: "30%" }} />
+    </div>
+  );
+}
+
+export default App;
