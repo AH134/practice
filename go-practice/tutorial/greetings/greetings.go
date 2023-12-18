8f08934970169fa649af218080c008b7f1ef8996
Andy <github.com.uo5n8@aleeas.com>
2023-12-17 22:54:42 -0800

    Completed Golang tutorial

--- go-practice/tutorial/greetings/greetings.go
+++ go-practice/tutorial/greetings/greetings.go
@@ -11,7 +11,8 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	return message, nil
 }
 
@@ -23,4 +24,18 @@ func randomFormat() string {
 	}
 
 	return formats[rand.Intn(len(formats))]
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+	return messages, nil
 }
--- go-practice/tutorial/hello/hello.go
+++ go-practice/tutorial/hello/hello.go
@@ -8,19 +8,18 @@ import (
 )
 
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	// request a greeting message
-	message, err := greetings.Hello("Bob")
-	// If an error was returned, print it to the console and
-	// exit the program.
+
+	names := []string{
+		"Gladys",
+		"Samantha",
+		"Darrin",
+	}
+	// Get a greeting message and print it.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// if no error was returned, print the returned message
-	// to the console
-	fmt.Println(message)
+	fmt.Println(messages)
 }
