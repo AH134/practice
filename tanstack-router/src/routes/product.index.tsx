40c99ef7931853c7924dc78df0f80839b767f4ff
Andy <github.com.uo5n8@aleeas.com>
2024-04-02 20:03:38 -0700

    Delete tanstack-router directory

--- tanstack-router/src/routes/product.index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/product/")({
-  component: Product,
-});
-function Product() {
-  return <div>Product selected goes her</div>;
-}
