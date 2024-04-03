2f2e1a931725b534ca8507ca29c217283c110fca
Andy <github.com.uo5n8@aleeas.com>
2024-04-02 20:03:31 -0700

    Delete tdl-svelte-react directory

--- tdl-svelte-react/react-client/src/types.ts
+++
@@ -1,7 +0,0 @@
-export type Item = {
-  id: number;
-  user_id: number;
-  title: string;
-  description: string;
-  status: boolean;
-};
--- tdl-svelte-react/server/models/todo.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Todo struct {
-	Id          int    `json:"id"`
-	UserId      int    `json:"user_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Status      bool   `json:"status"`
-}
