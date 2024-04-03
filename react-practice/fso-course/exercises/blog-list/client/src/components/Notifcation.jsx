9c922be6bb456a8374d01a523b3c7a96b026c4c9
Andy <github.com.uo5n8@aleeas.com>
2024-04-02 20:03:25 -0700

    Delete react-practice directory

--- react-practice/fso-course/exercises/blog-list/client/src/components/Notifcation.jsx
+++
@@ -1,9 +0,0 @@
-const Notification = ({ message, isSuccessful }) => {
-  if (message === null) {
-    return null;
-  }
-
-  return <div className={isSuccessful ? "success" : "error"}>{message}</div>;
-};
-
-export default Notification;
--- react-practice/fso-course/practice/client/cypress/e2e/notes.md
+++
@@ -1,4 +0,0 @@
-# how to fix cypress firefox axios ns binding error
-
-- add cy.wait(time) after every the refreshes the page
-  - test by deleting cy.wait() from anyline in code
