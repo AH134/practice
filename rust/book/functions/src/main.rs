3a145dbc903bbcfa99d588f5707d6222d3896e81
AH134 <github.com.uo5n8@aleeas.com>
2024-04-05 00:18:47 -0700

    finished 3.3-3.4

--- rust/book/functions/src/main.rs
+++ rust/book/functions/src/main.rs
@@ -1,9 +1,32 @@
 fn main() {
     println!("Hello, world!");
-
     another_function();
+    print_labeled_measurement(5, 'h');
+
+    // expressions and statements
+    // cannot do let x = let x = 5
+
+    let x = five();
+    println!("The value of x is: {x}");
+
+    let y = plus_one(5);
+    println!("The value of y is: {y}");
 }
 
 fn another_function() {
     println!("Another function.")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+// last line is the return value, no semicolon
+// or can return early by using the return keyword
+fn five() -> i32 {
+    5 // or return 5;
+}
+
+fn plus_one(y: i32) -> i32 {
+    y + 1;
 }
